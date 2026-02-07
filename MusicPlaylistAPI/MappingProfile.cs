673f9c00c0b7e7adaa9e0ff69f217a41eac02119
ibrkovic <ibrkovic989@gmail.com>
2026-02-07 15:58:02 +0100

    Added maps for CreateArtistDTO, CreateSongDTO, CreateGenreDTO and UpdateArtistDTO to MappingProfile.cs

--- MusicPlaylistAPI/MappingProfile.cs
+++ MusicPlaylistAPI/MappingProfile.cs
@@ -18,6 +18,37 @@ namespace MusicPlaylistAPI
                 .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
+            // ========== ARTIST MAPPINGS ==========
+            CreateMap<Artist, ArtistDTO>();
+            CreateMap<CreateArtistDTO, Artist>();
+            CreateMap<UpdateArtistDTO, Artist>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // ========== SONG MAPPINGS ==========
+            CreateMap<Song, SongDTO>()
+                .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.DurationInSeconds))
+                .ForMember(dest => dest.Genres,
+                    opt => opt.MapFrom(src => src.SongGenres.Select(sg => sg.Genre)))
+                .ForMember(dest => dest.Artist,
+                    opt => opt.MapFrom(src => src.Artist));
+
+            CreateMap<CreateSongDTO, Song>()
+                .ForMember(dest => dest.DurationInSeconds, opt => opt.MapFrom(src => src.Duration))
+                .ForMember(dest => dest.SongGenres, opt => opt.Ignore())
+                .ForMember(dest => dest.Artist, opt => opt.Ignore());
+
+            CreateMap<UpdateSongDTO, Song>()
+                .ForMember(dest => dest.DurationInSeconds, opt => opt.MapFrom(src => src.Duration ?? 0))
+                .ForMember(dest => dest.SongGenres, opt => opt.Ignore())
+                .ForMember(dest => dest.Artist, opt => opt.Ignore())
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            // ========== GENRE MAPPINGS ==========
+            CreateMap<Genre, GenreDTO>();
+            CreateMap<CreateGenreDTO, Genre>();
+            CreateMap<UpdateGenreDTO, Genre>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
             // ========== PLAYLIST MAPPINGS ==========
             CreateMap<Playlist, PlaylistDTO>()
                 .ForMember(dest => dest.Owner,
@@ -30,18 +61,10 @@ namespace MusicPlaylistAPI
                 .ForMember(dest => dest.PlaylistSongs, opt => opt.Ignore())
                 .ForMember(dest => dest.User, opt => opt.Ignore());
 
-            // ========== SONG MAPPINGS ==========
-            CreateMap<Song, SongDTO>()
-                .ForMember(dest => dest.Genres,
-                    opt => opt.MapFrom(src => src.SongGenres.Select(sg => sg.Genre)))
-                .ForMember(dest => dest.Artist,
-                    opt => opt.MapFrom(src => src.Artist));
-
-            // ========== ARTIST MAPPINGS ==========
-            CreateMap<Artist, ArtistDTO>();
-
-            // ========== GENRE MAPPINGS ==========
-            CreateMap<Genre, GenreDTO>();
+            CreateMap<UpdatePlaylistDTO, Playlist>()
+                .ForMember(dest => dest.PlaylistSongs, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
 
             // ========== REVERSE MAPPINGS ==========
             CreateMap<UserDTO, User>()
